ceb7eb8fb935a6f868134740c6b72b76aca7cdab
bukulele <kit.sazonov@gmail.com>
2021-11-02 16:33:06 +0400

    small fixes in internal files

--- src/soundSet1.js
+++ src/soundSet1.js
@@ -4,7 +4,7 @@ import tone1 from "./sounds/56827__esformouse__tone01.m4a";
 import guitarA from "./sounds/8450__speedy__a-full-up.m4a";
 import guitarAsus from "./sounds/8458__speedy__asus-full-ok.m4a";
 import guitarCadd9 from "./sounds/8473__speedy__cadd9-full-up.m4a";
-import chineese3 from "./sounds/209868__veiler__sabian-aax-chinese-3.m4a";
+import chinese3 from "./sounds/209868__veiler__sabian-aax-chinese-3.m4a";
 import hihat2 from "./sounds/133935__hesed23__hihat2.m4a";
 import tom16 from "./sounds/16320__ltibbits__tom-16-tight-high-vol.m4a";
 
@@ -15,7 +15,7 @@ const SET_1 = {
   "guitar A": guitarA,
   "guitar A SUS": guitarAsus,
   "guitar Cadd9": guitarCadd9,
-  chineese: chineese3,
+  chinese: chinese3,
   "hi-hat": hihat2,
   "tom 16": tom16,
 };
